0b61dc99e28b7671c2d1581f74a4d4f652b0f411
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-16 22:23:08 -0300

    prettier active

--- src/components/buttonDark/DarkMode.jsx
+++ src/components/buttonDark/DarkMode.jsx
@@ -1,38 +1,37 @@
 import { React, useState } from "react"
-import './button.css'
+import "./button.css"
 
 const ButtonDarkMode = (props) => {
-
   const darkMode = () => {
-    if(props.propiety === 'dark') {
-      document.querySelector('.header-container').classList.toggle('dark')
-      const opcion = document.querySelectorAll('.opcion')
-      opcion.forEach(item => {
-        item.classList.toggle('dark')
+    if (props.propiety === "dark") {
+      document.querySelector(".header-container").classList.toggle("dark")
+      const opcion = document.querySelectorAll(".opcion")
+      opcion.forEach((item) => {
+        item.classList.toggle("dark")
       })
-      document.querySelector('.container-img').classList.toggle('dark')
-      document.querySelector('.container-chats').classList.toggle('dark')
-      document.querySelector('.container-search').classList.toggle('dark')
-      document.querySelector('.container-add-chat').classList.toggle('dark')
-      document.querySelector('.container-chat-nav').classList.toggle('dark')
-      document.querySelector('.container-info-user').classList.toggle('dark')
-      document.querySelector('.container-icons').classList.toggle('dark')
-      document.querySelector('.container-input-msg').classList.toggle('dark')
-      document.querySelector('.container-emotes').classList.toggle('dark')
-      document.querySelector('.container-chat-msg').classList.toggle('dark')
+      document.querySelector(".container-img").classList.toggle("dark")
+      document.querySelector(".container-chats").classList.toggle("dark")
+      document.querySelector(".container-search").classList.toggle("dark")
+      document.querySelector(".container-add-chat").classList.toggle("dark")
+      document.querySelector(".container-chat-nav").classList.toggle("dark")
+      document.querySelector(".container-info-user").classList.toggle("dark")
+      document.querySelector(".container-icons").classList.toggle("dark")
+      document.querySelector(".container-input-msg").classList.toggle("dark")
+      document.querySelector(".container-emotes").classList.toggle("dark")
+      document.querySelector(".container-chat-msg").classList.toggle("dark")
     } else {
-      document.querySelector('.header-container').classList.remove('dark')
-      document.querySelector('.opcion').classList.remove('dark')
-      document.querySelector('.container-img').classList.remove('dark')
-      document.querySelector('.container-chats').classList.remove('dark')
-      document.querySelector('.container-search').classList.remove('dark')
-      document.querySelector('.container-add-chat').classList.remove('dark')
-      document.querySelector('.container-chat-nav').classList.remove('dark')
-      document.querySelector('.container-info-user').classList.remove('dark')
-      document.querySelector('.container-icons').classList.remove('dark')
-      document.querySelector('.container-input-msg').classList.remove('dark')
-      document.querySelector('.container-emotes').classList.remove('dark')
-      document.querySelector('.container-chat-msg').classList.remove('dark')
+      document.querySelector(".header-container").classList.remove("dark")
+      document.querySelector(".opcion").classList.remove("dark")
+      document.querySelector(".container-img").classList.remove("dark")
+      document.querySelector(".container-chats").classList.remove("dark")
+      document.querySelector(".container-search").classList.remove("dark")
+      document.querySelector(".container-add-chat").classList.remove("dark")
+      document.querySelector(".container-chat-nav").classList.remove("dark")
+      document.querySelector(".container-info-user").classList.remove("dark")
+      document.querySelector(".container-icons").classList.remove("dark")
+      document.querySelector(".container-input-msg").classList.remove("dark")
+      document.querySelector(".container-emotes").classList.remove("dark")
+      document.querySelector(".container-chat-msg").classList.remove("dark")
     }
   }
 
--- src/components/login/Profile.jsx
+++ src/components/login/Profile.jsx
@@ -2,43 +2,48 @@ import React from "react"
 import { useAuth0 } from "@auth0/auth0-react"
 
 export const ProfileUser = () => {
-    const { user, isAuthenticated, isLoading } = useAuth0()
+  const { user, isAuthenticated, isLoading } = useAuth0()
 
-    if(isLoading) {
-        return(
-            <div className="container-profile">
-                <h1>Loading<span className="loader"></span></h1>
-            </div>
-        )
-    }
-
-    return(
-        isAuthenticated && (
-            <div className="container-info-mail">
-                <img className="img-mail" src={user.picture} alt={user.name} />
-                <div className="container-info-personal">
-                    <h2 className="name">Nombre: {user.name}</h2>
-                    <p className="email">
-                        Email: {user.email}
-                    </p>
-                    <input type="button" value="Edit Profile" className="btn-edit-profile" onClick={toggleProfile} />
-                    <div className="container-edit-profile">
-                        <textarea 
-                            name="textarea" 
-                            placeholder="Descripcion..." 
-                            className="textarea" 
-                            cols="36" 
-                            rows="10"
-                        >
-
-                        </textarea>
-                        <div className="container-modified">
-                            <button className="btn-modified-edit">Save</button>
-                            <button className="btn-modified-delete"><i class="ri-delete-bin-line"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+  if (isLoading) {
+    return (
+      <div className="container-profile">
+        <h1>
+          Loading<span className="loader"></span>
+        </h1>
+      </div>
     )
+  }
+
+  return (
+    isAuthenticated && (
+      <div className="container-info-mail">
+        <img className="img-mail" src={user.picture} alt={user.name} />
+        <div className="container-info-personal">
+          <h2 className="name">Nombre: {user.name}</h2>
+          <p className="email">Email: {user.email}</p>
+          <input
+            type="button"
+            value="Edit Profile"
+            className="btn-edit-profile"
+            onClick={toggleProfile}
+          />
+          <div className="container-edit-profile">
+            <textarea
+              name="textarea"
+              placeholder="Descripcion..."
+              className="textarea"
+              cols="36"
+              rows="10"
+            ></textarea>
+            <div className="container-modified">
+              <button className="btn-modified-edit">Save</button>
+              <button className="btn-modified-delete">
+                <i class="ri-delete-bin-line"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  )
 }
