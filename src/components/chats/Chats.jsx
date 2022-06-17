a2217ed96ea48cc1307aa503337c414f14688102
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2022-06-16 23:55:28 -0300

    changes thins place

--- src/components/chats/Chats.jsx
+++ src/components/chats/Chats.jsx
@@ -1,19 +1,16 @@
-import { React, useState } from 'react'
-import './chats.css'
+import { React, useState } from "react"
+import "./chats.css"
 
 const Chats = () => {
   return (
     <main className="container-chats">
       <div className="container-content">
         <div className="container-search">
-          <input 
-            type="text" 
-            className='busqueda'
-            max={"20"} />
+          <input type="text" className="busqueda" max={"20"} />
           <i class="ri-search-2-line"></i>
         </div>
         <div className="container-add-chat">
-          <button className='btn-add-chat'>Add new chat...</button>
+          <button className="btn-add-chat">Add new chat...</button>
         </div>
       </div>
     </main>
